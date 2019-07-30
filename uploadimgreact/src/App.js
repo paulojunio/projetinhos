83efd57fbe36608e37d320d8b6d4f0eb8e7f3cd1
Paulo Junio Reis Rodrigues <pjrrodrigues@sga.pucminas.br>
2019-07-30 09:55:36 -0300

    Fiz errado

--- uploadimgreact/src/App.js
+++ uploadimgreact/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { eventNames } from 'cluster';
+//import { isNoop } from '@babel/types';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    selectedFile: null
+  }
+  fileSelectedHandle = event => {
+    selectedFile: event.target.files[0]
+  }
+
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    axios.post('');
+  }
+  render() {
+    return (
+      <div className="App">
+          <input type='file' onChange={this.fileSelectedHandle}></input>
+          <button onClick={this.fileUploadHandler}>Upload</button>
+      </div>
+    );
+  }
 }
-
 export default App;
