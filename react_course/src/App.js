8757be3df6eb6343b948d6794140502a43368d8c
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-06-04 23:34:36 -0300

    Aprendendo hooks dnv

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersons] = useState({
     persons : [
       { name: "Paulo", age: 22},
       { name: "Juliana", age: 26},
       { name: "Juninho", age: 29}
     ]
-  }
+  });
 
-  switchNameHandle = () => {
-    // console.log("Hi?");
-    this.setState({
+  const [otherState, setOtherState] = useState("So uma palavrinhas");
+
+  const switchNameHandle = () => {
+    setPersons({
       persons : [
         { name: "Paulo Junio", age: 22},
         { name: "Juliana", age: 26},
@@ -22,17 +23,19 @@ class App extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <p>Hello World???</p>
-        <button onClick={this.switchNameHandle}>Click here!!</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>A fita ta facil</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>A fita é complicada</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[2].age}>A fita é a fita</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hello World???</h1>
+      <p>{otherState}</p>
+      <button onClick={switchNameHandle}>Click here!!</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>A fita ta facil</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>A fita é complicada</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[2].age}>A fita é a fita</Person>
+    </div>
+  );
 }
 
 export default App;
+
+
+
