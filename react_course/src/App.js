706f5636e0d8c795ef10edd16a3c587f86a8da01
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-06-05 00:01:18 -0300

    seção 3 terminada

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -13,11 +13,21 @@ const App = props => {
 
   const [otherState, setOtherState] = useState("So uma palavrinhas");
 
-  const switchNameHandle = () => {
+  const switchNameHandle = (newName) => {
     setPersons({
       persons : [
-        { name: "Paulo Junio", age: 22},
+        { name: newName, age: 22},
         { name: "Juliana", age: 26},
+        { name: "Juninho", age: 50}
+      ]
+    });
+  }
+
+  const nameChangeHandle = (event) => {
+    setPersons({
+      persons : [
+        { name: "Paulo", age: 22},
+        { name: event.target.value , age: 26},
         { name: "Juninho", age: 50}
       ]
     });
@@ -27,10 +37,15 @@ const App = props => {
     <div className="App">
       <h1>Hello World???</h1>
       <p>{otherState}</p>
-      <button onClick={switchNameHandle}>Click here!!</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>A fita ta facil</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>A fita é complicada</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[2].age}>A fita é a fita</Person>
+      <button onClick={() => switchNameHandle("Paulinho")}>Click here!!</button>
+      {personsState.persons.map( person => {
+        return (
+          <Person name={person.name} age={person.age} click={switchNameHandle.bind(this, "Paulao")} change={nameChangeHandle}></Person>
+        )})
+      }
+      {/* <Person name={personsState.persons[0].name} age={personsState.persons[0].age} click={switchNameHandle.bind(this, "Paulao")} change={nameChangeHandle}>A fita ta facil</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandle}>A fita é complicada</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} click={switchNameHandle}>A fita é a fita</Person> */}
     </div>
   );
 }
--- react_course/src/Person/Person.js
+++ react_course/src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Person = (props) => {
     return (
-        <div>
+        <div onClick={props.click}>
             <p>Opa olha eu vivo com o nomezinho de {props.name} e idade de {props.age}, to na fita</p>
             <p>{props.children}</p>
+            <input type='text' onChange={props.change}/>
         </div>
     );
 }
