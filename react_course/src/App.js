dec93a6a287b8f5f3095778bac8cbc7d0488a661
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-06-06 23:45:25 -0300

    Seção 4

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -1,48 +1,80 @@
 import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 const App = props => {
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+  const [showPersonsState, setShowPersons] = useState(false);
+
   const [personsState, setPersons] = useState({
     persons : [
-      { name: "Paulo", age: 22},
-      { name: "Juliana", age: 26},
-      { name: "Juninho", age: 29}
+      { id: 1, name: "Paulo", age: 22},
+      { id: 2, name: "Juliana", age: 26},
+      { id: 3, name: "Juninho", age: 29}
     ]
   });
 
-  const [otherState, setOtherState] = useState("So uma palavrinhas");
+  const [otherState] = useState("So uma palavrinhas");
 
-  const switchNameHandle = (newName) => {
+  // const switchNameHandle = (newName) => {
+  //   setPersons({
+  //     persons : [
+  //       { name: newName, age: 22},
+  //       { name: "Juliana", age: 26},
+  //       { name: "Juninho", age: 50}
+  //     ]
+  //   });
+  // }
+
+  const deletePerson = (indexPerson) => {
+    // let resultPersons = personsState.persons; //Pratica ruim, usando ponteiro do array do state para manipular o array
+    // let resultPersons = personsState.persons.slice(); //Copia do array para manipula-lo
+    let resultPersons = [...personsState.persons]; //Maneira mais morderna de copiar um array usando o "Spread"
+    resultPersons.splice(indexPerson, 1);
+    setPersons({persons: resultPersons});
+  }
+
+  const nameChangeHandle = (event, id) => {
+    const personIndex = personsState.persons.findIndex( p => {
+      return p.id === id;
+    });
+    let resultPersons = [...personsState.persons];
+    resultPersons[personIndex].name = event.target.value;
     setPersons({
-      persons : [
-        { name: newName, age: 22},
-        { name: "Juliana", age: 26},
-        { name: "Juninho", age: 50}
-      ]
+      persons : resultPersons
     });
   }
 
-  const nameChangeHandle = (event) => {
-    setPersons({
-      persons : [
-        { name: "Paulo", age: 22},
-        { name: event.target.value , age: 26},
-        { name: "Juninho", age: 50}
-      ]
-    });
+  const togglePersonsHandler = () => {
+    setShowPersons(!showPersonsState);
+  }
+
+  let persons = null;
+
+  if(showPersonsState) {
+    persons = ( <div>
+        {personsState.persons.map( (person, index) => {
+          return (
+            <Person name={person.name} age={person.age} click={() => deletePerson(index)} change={(event) => nameChangeHandle(event, person.id)} key={person.id}></Person>
+          )})
+        }
+    </div>
+    );
   }
 
   return (
     <div className="App">
       <h1>Hello World???</h1>
       <p>{otherState}</p>
-      <button onClick={() => switchNameHandle("Paulinho")}>Click here!!</button>
-      {personsState.persons.map( person => {
-        return (
-          <Person name={person.name} age={person.age} click={switchNameHandle.bind(this, "Paulao")} change={nameChangeHandle}></Person>
-        )})
-      }
+      <button style={style} onClick={togglePersonsHandler}>Click here!!</button>
+      {persons}
       {/* <Person name={personsState.persons[0].name} age={personsState.persons[0].age} click={switchNameHandle.bind(this, "Paulao")} change={nameChangeHandle}>A fita ta facil</Person>
       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandle}>A fita é complicada</Person>
       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} click={switchNameHandle}>A fita é a fita</Person> */}
--- react_course/src/Person/Person.js
+++ react_course/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const Person = (props) => {
     return (
-        <div onClick={props.click}>
-            <p>Opa olha eu vivo com o nomezinho de {props.name} e idade de {props.age}, to na fita</p>
+        <div className="Person">
+            <p onClick={props.click} >Opa olha eu vivo com o nomezinho de {props.name} e idade de {props.age}, to na fita</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.change}/>
         </div>
